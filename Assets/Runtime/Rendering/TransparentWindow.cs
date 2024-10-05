8db3a409b1fecc1d16e45598aa7652a8071239d3
Rabbit <58245404+PlusOneRabbit@users.noreply.github.com>
2024-10-04 23:28:36 -0700

    Change clear color to magenta

--- Assets/Runtime/Rendering/TransparentWindow.cs
+++ Assets/Runtime/Rendering/TransparentWindow.cs
@@ -1,8 +1,9 @@
-// DO NOT REMOVE THESE REFERENCES ITS NEEDED FOR THE SCRIPT WHEN COMPILING
+#if !UNITY_EDITOR && UNITY_STANDALONE_WIN
 using System;
 using System.Runtime.InteropServices;
-using UnityEngine;
+#endif
 
+using UnityEngine;
 
 public class TransparancyManager : MonoBehaviour
 {
@@ -24,27 +25,29 @@ public class TransparancyManager : MonoBehaviour
 
     private const int GWL_EXSTYLE = -20;
     private const int WS_EX_LAYERED = 0x80000;
-    // Remove WS_EX_TRANSPARENT to allow window interaction
+    private const int WS_EX_TRANSPARENT = 0x20;
     private const int LWA_COLORKEY = 0x1;
     private const int LWA_ALPHA = 0x2;
     private const int HWND_TOPMOST = -1;
     private const int SWP_FRAMECHANGED = 32 | 64;
 
+    private IntPtr hWnd;
+
     private void Start()
     {
         // Get active window handle
-        var hwnd = GetActiveWindow();
+        hWnd = GetActiveWindow();
 
         // Make window layered but not input-transparent
-        SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+        SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
 
         // Adjust color key and alpha for selective transparency
-        SetLayeredWindowAttributes(hwnd, 0, 255, LWA_COLORKEY); // You might not need LWA_COLORKEY
+        SetLayeredWindowAttributes(hWnd, 0xFF00FF, 255, LWA_COLORKEY); // You might not need LWA_COLORKEY
 
         // Make window appear above all others
         var fWidth = Screen.width;
         var fHeight = Screen.height;
-        SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, fWidth, fHeight, SWP_FRAMECHANGED);
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, fWidth, fHeight, SWP_FRAMECHANGED);
     }
 #endif
 }
