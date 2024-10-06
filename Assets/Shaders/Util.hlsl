eae8ea51377249502eaa5bc7c43cdeba98970fb1
Rabbit <58245404+PlusOneRabbit@users.noreply.github.com>
2024-10-06 16:54:25 -0700

    Increase mood gain for window riding and cursor

--- Assets/Shaders/Util.hlsl
+++ Assets/Shaders/Util.hlsl
@@ -1,3 +1,7 @@
 float temporalLerp(float a, float b, float t, float delta) {
 	return lerp(a, b, 1.0 - pow(1.0 - t, delta * 60.0));
 }
+
+float floatDist(float a, float b) {
+    return abs(a - b);
+}
