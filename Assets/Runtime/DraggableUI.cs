22f2c04583a41d0397d9a03f5e15bc5d045a24a9
Rabbit <58245404+PlusOneRabbit@users.noreply.github.com>
2024-10-06 21:12:17 -0700

    Add drag bar for game UI

--- Assets/Runtime/DraggableUI.cs
+++ Assets/Runtime/DraggableUI.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DraggableUI : MonoBehaviour, IDragHandler
+public class DraggableUI : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    private bool isDragging = false;
     public void OnDrag(PointerEventData eventData) {
-        transform.position += new Vector3(eventData.delta.x, eventData.delta.y, 0.0f);
+        if (isDragging) {
+            transform.position += new Vector3(eventData.delta.x, eventData.delta.y, 0.0f);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        if (eventData.pointerCurrentRaycast.gameObject == gameObject) {
+            isDragging = true;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        isDragging = false;
     }
 }
